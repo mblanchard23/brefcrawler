85038705d7b411297183e91b7b9aa279075b1309
matt <mblanchard23@gmail.com>
2018-01-15 12:25:01 +0000

    Season level data on player page

--- player_page.py
+++ player_page.py
@@ -1,8 +1,30 @@
+''' Get Per Season Level data from the Player Page '''
+
 import requests, pandas
 from bs4 import BeautifulSoup
 import sqlite3
 
-baseurl = "http://www.basketball-reference.com/players/{firstletter}/{playerid}.html"
+def get_tables_from_html_text(ht_as_bs_tag):
+	tables = ['<table class' + x for x in  str(ht_as_bs_tag).split('<table class')]
+	t2 = [t.split('</table>')[0] + '</table>' for t in tables]
+	soups = [BeautifulSoup(t,'html.parser') for t in t2]
+	soup_dict = {q.table.get('id'):pandas.read_html(str(q)) for q in soups if q}
+	dataframes = [pandas.read_html(t) for t in t2] 
+	return soups, dataframes, soup_dict
+
+def get_player_tables(player_id):
+	baseurl = "http://www.basketball-reference.com/players/{firstletter}/{playerid}.html"
+	req = requests.get(baseurl.format(firstletter=player_id[:1],playerid=player_id))
+	return get_tables_from_html_text( BeautifulSoup(req.text,'html.parser'))
+
+def get_player_html(player_id):
+	baseurl = "http://www.basketball-reference.com/players/{firstletter}/{playerid}.html"
+	return requests.get(baseurl.format(firstletter=player_id[:1],playerid=player_id))
+
+
+
+def test():
+	return get_player_tables('bryanko01')
 
 class player_page:
 
